c4c257280eed2dc3e12f95d560d2a11276bfce65
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-12-11 10:51:00 -0600

    add comments

--- file-lister.js
+++ file-lister.js
@@ -18,39 +18,39 @@ function jsFiles(path, outFilename, cb) {
     })
   })
 }
-
+// Create Arrays list
 jsFiles('./js/arrays', 'array-list.js', (message) => {
     console.log(message)
 })
-
+// Create Dates list
 jsFiles('./js/date', 'date-list.js', (message) => {
     console.log(message)
 })
-
+// Create DOM list
 jsFiles('./js/dom', 'dom-list.js', (message) => {
     console.log(message)
 })
-
+// Create Functions list
 jsFiles('./js/functions', 'functions-list.js', (message) => {
     console.log(message)
 })
-
+// Create JSON list
 jsFiles('./js/json', 'json-list.js', (message) => {
     console.log(message)
 })
-
+// Create Math list
 jsFiles('./js/math', 'math-list.js', (message) => {
     console.log(message)
 })
-
+// Create Object list
 jsFiles('./js/objects', 'objects-list.js', (message) => {
     console.log(message)
 })
-
+// Create RegEx list
 jsFiles('./js/regex', 'regex-list.js', (message) => {
     console.log(message)
 })
-
+// Create String list
 jsFiles('./js/strings', 'strings-list.js', (message) => {
     console.log(message)
 })
