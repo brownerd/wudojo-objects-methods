53e8473db28285ed1c14521f88abd9b245530a84
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-12-12 17:15:27 -0600

    added tape files

--- test/index.js
+++ test/index.js
@@ -1,3 +1,12 @@
 // Require all test here in one place
 
+require('./arrays_tape.js')
+require('./date_tape.js')
+require('./dom_tape.js')
+require('./functions_tape.js')
+require('./json_tape.js')
+require('./math_tape.js')
+require('./objects_tape.js')
+require('./other_tape.js')
+require('./regex_tape.js')
 require('./strings_tape.js')
---
+++ test/other_tape.js
@@ -0,0 +1,17 @@
+import test from 'tape'
+// import {len} from '../js/other/01-.js'
+
+/*
+// For each unit test you write
+// answer these questions
+
+test('What component aspect are you testing?', assert => {
+
+  const actual = 'What is the actual output?'
+  const expected = 'What is the expected output'
+
+  assert.equal( actual, expected, 'What should the feature do?')
+
+  assert.end()
+})
+*/
