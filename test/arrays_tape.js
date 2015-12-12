0ca5c790e42ad0c504ee958b13f48792e67ea8b3
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-12-12 17:21:48 -0600

    add test for arrays

--- test/arrays_tape.js
+++ test/arrays_tape.js
@@ -1,5 +1,14 @@
 import test from 'tape'
-// import {len} from '../js/arrays/01-Array.length.js'
+import {len} from '../js/arrays/01-Array.prototype.length.js'
+
+test('len() function returns correct length of array', assert => {
+
+  const actual = len([1, 2, 3])
+  const expected = 3
+
+  assert.equal(actual, expected, `len([1, 2, 3]) should return ${expected}`)
+  assert.end()
+})
 
 /*
 // For each unit test you write
@@ -10,7 +19,7 @@ test('What component aspect are you testing?', assert => {
   const actual = 'What is the actual output?'
   const expected = 'What is the expected output'
 
-  assert.equal( actual, expected, 'What should the feature do?')
+  assert.equal(actual, expected, 'What should the feature do?')
 
   assert.end()
 })
