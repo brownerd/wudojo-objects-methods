aae0adcc8e50688a1999be073d614154e5de873a
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-12-15 15:15:49 -0600

    not making normalize function, dont know when Id ever use it

---
+++ js/strings/16-String.prototype.normalize.js
@@ -0,0 +1,7 @@
+// String.prototype.normalize()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+
+// ES6
+
+// I don't really understand how this helps yet.
+// I could make the function, but I don't know where or if I'd ever use it.
